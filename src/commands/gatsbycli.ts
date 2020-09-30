f797c2f8d487d5b3a0d2f7edd9448fec729d8f2e
Joonyoung <impervious7@gmail.com>
2020-09-30 15:42:08 -0700

    (add) implement password inputbox
    Co-authored-by: DylanRH <dylan@hensel.com>
    Co-authored-by: Mig824 <migmic93@gmail.com>
    Co-authored-by: bhayashi <BrianMHayashi@gmail.com>
    Co-authored-by: lucycchi <lucycchi@gmail.com>
    “

--- src/commands/gatsbycli.ts
+++ src/commands/gatsbycli.ts
@@ -5,11 +5,21 @@ import Utilities from '../utils/Utilities';
 
 export default class GatsbyCli {
   // no need to instantiate to use this method in extenstion.ts
-  static installGatsby() {
+  static async installGatsby() {
     // if a gatsby terminal isn't open, create a new terminal. Otherwise, use gatsbyhub terminal
     const activeTerminal = Utilities.getActiveTerminal();
 
     activeTerminal.sendText('sudo npm install -g gatsby-cli');
+    // NOTE: comeback to this
+    // if admin password is required:
+      // Creates an inputbox for password when install gatsby button is clicked
+      const inputPassword = await vscode.window.showInputBox({ password: true, placeHolder: 'Input administrator password' });
+      activeTerminal.sendText(inputPassword);
+
+      // if the password is wrong, show inputbox again
+      
+    // else, show terminal
+
     activeTerminal.show();
   }
 }
