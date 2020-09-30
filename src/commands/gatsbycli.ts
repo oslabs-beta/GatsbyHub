10277133fbdf5cfd667679f027b02803a7534d9a
Joonyoung <impervious7@gmail.com>
2020-09-30 16:58:49 -0700

    (add) partially implement createSite button
    Co-authored-by: DylanRH <dylan@hensel.com>
    Co-authored-by: Mig824 <migmic93@gmail.com>
    Co-authored-by: bhayashi <BrianMHayashi@gmail.com>
    Co-authored-by: lucycchi <lucycchi@gmail.com>

--- src/commands/gatsbycli.ts
+++ src/commands/gatsbycli.ts
@@ -4,7 +4,9 @@ import * as vscode from 'vscode';
 import Utilities from '../utils/Utilities';
 
 export default class GatsbyCli {
-  // no need to instantiate to use this method in extenstion.ts
+  // installs gatsby-cli for the user when install gatsby button is clicked
+
+  //  static keyword: eliminates the need to instantiate to use this method in extenstion.ts
   static async installGatsby() {
     // if a gatsby terminal isn't open, create a new terminal. Otherwise, use gatsbyhub terminal
     const activeTerminal = Utilities.getActiveTerminal();
@@ -19,6 +21,24 @@ export default class GatsbyCli {
       // if the password is wrong, show inputbox again
       
     // else, show terminal
+
+    activeTerminal.show();
+  }
+
+  /**  creates a new site when 'Create New Site' button is clicked
+   * currently uses default gatsby starter, but uses gatsby new url. see https://www.gatsbyjs.com/docs/gatsby-cli/
+   * NOTE: new site will be created wherever the root directory is currently located
+   * the user terminal should be at the directory user wishes to download the files.
+   */
+  static async createSite() {
+    const activeTerminal = Utilities.getActiveTerminal();
+
+    // problem: does not work when creating a new folder
+    const root = await vscode.commands.executeCommand('vscode.openFolder');
+    console.log(root);
+
+    const siteName = await vscode.window.showInputBox({ placeHolder: 'Input new site name' });
+    activeTerminal.sendText(`gatsby new ${siteName}`);
 
     activeTerminal.show();
   }
