13a503231b79d0edfd3c394bbaf128513f067e00
b-hash <brianmhayashi@gmail.com>
2020-09-30 20:04:48 -0700

    (add) statusBarItem and partial develop button
    Co-authored-by: DylanRH <dylan@hensel.com>
    Co-authored-by: Mig824 <migmic93@gmail.com>
    Co-authored-by: JoonyoungKim025 <impervious7@gmail.com>
    Co-authored-by: lucycchi <lucycchi@gmail.com>

--- src/utils/Utilities.ts
+++ src/utils/Utilities.ts
@@ -1,20 +1,20 @@
 // Helper functions for gatsbycli.ts
 import * as vscode from 'vscode';
 
-interface Terminal {
-  sendText: any;
-  show: any;
-  name: string;
-}
+// interface Terminal {
+//   sendText: any;
+//   show: any;
+//   name: string;
+// }
 
 export default class Utilities {
   static getActiveTerminal() {
     const { terminals } = vscode.window;
     const filtered = terminals.filter(
-      (obj: Terminal) => obj.name === 'gatsbyhub',
+      (obj: vscode.Terminal) => obj.name === 'gatsbyhub',
     );
 
-    let terminal: Terminal;
+    let terminal: vscode.Terminal;
 
     // if there is no gatsby terminal running, create one
     if (filtered.length === 0) {
