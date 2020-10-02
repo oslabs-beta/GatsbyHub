6d1299931594e1ff63c0b4ea6b958515b1c14b6f
mig824 <migmic93@gmail.com>
2020-10-01 22:11:44 -0700

    (add) createTreeView for plugins view

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable import/extensions */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import GatsbyCli from './commands/gatsbycli';
 import StatusBar from './utils/statusBarItem';
+import PluginProvider from './models/PluginProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate() {
   vscode.commands.registerCommand(
     // package.json command
     'gatsbyhub.installGatsby',
-    GatsbyCli.installGatsby,
+    GatsbyCli.installGatsby
   );
 
   vscode.commands.registerCommand('gatsbyhub.createSite', GatsbyCli.createSite);
 
   vscode.commands.registerCommand(
     'gatsbyhub.developServer',
-    GatsbyCli.developServer,
+    GatsbyCli.developServer
   );
 
   StatusBar.createStatusBarItem();
+
+  vscode.window.createTreeView('plugins', {
+    treeDataProvider: new PluginProvider(),
+  });
   //   context.subscriptions.push(sb);
 }
 
