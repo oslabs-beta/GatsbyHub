bc88fccea7a1f5601342d763cc6c740e64f95761
mig824 <migmic93@gmail.com>
2020-10-02 14:40:35 -0700

    (add) more plugins to display in plugin view

--- src/models/PluginProvider.ts
+++ src/models/PluginProvider.ts
@@ -6,9 +6,24 @@ export default class PluginProvider implements vscode.TreeDataProvider<Plugin> {
 
   constructor() {
     this.data = [
-      new Plugin('gatsby-plugin-sass'),
-      new Plugin('gatsby-plugin-sass'),
-      new Plugin('gatsby-plugin-sass'),
+      new Plugin('gatsby-source-filesystem'),
+      new Plugin('gatsby-plugin-react-helmet'),
+      new Plugin('gatsby-plugin-sharp'),
+      new Plugin('gatsby-plugin-manifest'),
+      new Plugin('gatsby-image'),
+      new Plugin('gatsby-plugin-page-creator'),
+      new Plugin('gatsby-plugin-typescript'),
+      new Plugin('gatsby-transformer-sharp'),
+      new Plugin('gatsby-plugin-offline'),
+      new Plugin('gatsby-plugin-styled-components'),
+      new Plugin('gatsby-remark-images'),
+      new Plugin('gatsby-plugin-mdx'),
+      new Plugin('gatsby-transformer-remark'),
+      new Plugin('gatsby-plugin-sitemap'),
+      new Plugin('gatsby-plugin-sass', [
+        new Plugin('Peer Dependencies', [new Plugin('node-sass')]),
+        new Plugin('Docs', [new Plugin('Link to Docs')]),
+      ]),
     ];
   }
 
