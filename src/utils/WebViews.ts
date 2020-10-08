1e8d9144188687d05e92e76b0558c792a388ca38
b-hash <brianmhayashi@gmail.com>
2020-10-07 17:01:06 -0700

    (update) webview with static header

--- src/utils/WebViews.ts
+++ src/utils/WebViews.ts
@@ -4,26 +4,42 @@ import PluginData from '../models/PluginData';
 // import react from "React";
 
 export default class PluginWebView {
-  static async openPluginWebView({ links, name }) {
-    /*     console.log(name);
-    console.log(links.homepage); */
-    console.log(
-      'in plugin web view',
-      await PluginData.getNpmInstall(links.repository, links.homepage),
-    );
+  static async openPluginWebView({ links, name, version, description }) {
+    const readMe = await PluginData.mdToHtml(links.repository, links.homepage);
+    const title = name
+      .replace(/-/g, ' ')
+      .replace(/^\w?|\s\w?/g, (match: string) => match.toUpperCase());
     // createWebviewPanel takes in the type of the webview panel & Title of the panel & showOptions
     const panel = vscode.window.createWebviewPanel(
       'plugin',
-      `Gatsby Plugin: ${name}`,
+      `Gatsby Plugin: ${title}`,
       vscode.ViewColumn.One,
       { enableScripts: true },
     );
-    const readMe = await PluginData.mdToHtml(links.repository, links.homepage);
 
     panel.webview.html = `
-    <div>
+    <style>
+      .plugin-header {
+        position: fixed;
+        top: 0;
+        background-color: var(--vscode-editor-background);
+        width: 100vw;
+      }
+      body {
+        position: absolute;
+        top: 9rem;
+      }
+    </style>
+    <div class="plugin-header">
+      <h1>${title}</h1>
+      <p>Version: ${version}</p>
+      <p>${description}</p>
+      <hr class="solid">
     </div>
-    ${readMe}`;
+    <div class="readme">
+      ${readMe}
+    </div>
+    `;
   }
 
   static installPlugin() {
