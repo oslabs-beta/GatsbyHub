fb194fa320cfe14b563273ccff194493a9bbe885
b-hash <brianmhayashi@gmail.com>
2020-10-08 14:56:06 -0700

    (update) refactor extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-
 import { ExtensionContext, commands, window } from 'vscode';
 import GatsbyCli from './commands/gatsbycli';
 import PluginProvider from './models/PluginProvider';
