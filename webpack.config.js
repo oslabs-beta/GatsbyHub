26c1dd8a77001695b4e27f569a13de83b09bde31
Dylan Hensel <dylan@hensel.com>
2020-10-16 12:25:58 -0700

    (add) compiler options to webpack.config file and updated package.json with necessary dev dependancies for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,10 @@ const path = require('path');
 
 // @type {import('webpack').Configuration}
 const config = {
-  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  target: 'node', // vscode extensions run in a Node.js-context 
+  entry: './src/extension.ts', 
   output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    // the bundle is stored in the 'dist' folder (check package.json)
     path: path.resolve(__dirname, 'dist'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2',
@@ -18,10 +17,10 @@ const config = {
   },
   devtool: 'source-map',
   externals: {
-    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    // support reading TypeScript and JavaScript files
     extensions: ['.ts', '.js']
   },
   module: {
@@ -31,7 +30,12 @@ const config = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'ts-loader'
+            loader: 'ts-loader',
+            options: {
+                compilerOptions: {
+                    "module": "es6" // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
+                }
+            }
           }
         ]
       }
