4a5ec48a65dbe418f38e3b72e24db680335841ec
mig824 <migmic93@gmail.com>
2020-10-17 23:44:18 -0700

    (update) context references the label instead of passing in another string

--- src/models/CommandProvider.ts
+++ src/models/CommandProvider.ts
@@ -1,18 +1,19 @@
 import { TreeDataProvider } from 'vscode';
 import CLICommand from './Commands';
 
-export default class CommandProvider implements TreeDataProvider<CLICommand> {
+export default class CLICommandProvider
+	implements TreeDataProvider<CLICommand> {
 	data: CLICommand[];
 
 	constructor() {
 		this.data = [
-			new CLICommand('New', 'new'),
-			new CLICommand('Develop', 'develop'),
-			new CLICommand('Build', 'build'),
-			new CLICommand('Serve', 'serve'),
-			new CLICommand('Info', 'info'),
-			new CLICommand('Clean', 'clean'),
-			new CLICommand('Install', 'install'),
+			new CLICommand('New'),
+			new CLICommand('Develop'),
+			new CLICommand('Build'),
+			new CLICommand('Serve'),
+			new CLICommand('Info'),
+			new CLICommand('Clean'),
+			new CLICommand('Install'),
 		];
 	}
 
