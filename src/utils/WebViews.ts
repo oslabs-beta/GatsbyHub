0d3ca86ad304e77a3879a73f4bf98a7f3fab72f3
Dylan Hensel <dylan@hensel.com>
2020-10-19 15:39:05 -0700

    (updates) Plugins readme to be cleaner and more informative for users

--- src/utils/WebViews.ts
+++ src/utils/WebViews.ts
@@ -93,11 +93,11 @@ export default class WebViews {
 	}
 
 	// Open webview readme of Plugin docs -- has to be seperate function because the command has to be pushed to subscriptions in extension.ts file -- same for starters and themes
-	static async openPluginDocs() {
-		const url =
-			'https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/plugins.md';
-		const response = await got(url);
-		const readMe = marked(response.body);
+	static openPluginDocs() {
+		// const url =
+		// 	'https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/plugins.md';
+		// const response = await got(url);
+		// const readMe = marked(response.body);
 
 		const panel = window.createWebviewPanel(
 			'Plugin Docs',
@@ -105,7 +105,31 @@ export default class WebViews {
 			ViewColumn.One
 		);
 
-		panel.webview.html = `${readMe}`;
+		panel.webview.html = `
+		<h1>Plugins</h2> 
+		<h3>Gatsby plugins are Node.js packages that implement Gatsby APIs. For larger, more complex sites, plugins let you modularize your site customizations into site-specific functionality.</h3>
+		<p>
+		One of the best ways to add functionality to Gatsby is through our plugin system. Gatsby is designed to be extensible, which means plugins are able to extend and modify just about everything Gatsby does.
+		<br>
+		Of the many possibilities, plugins can:
+		<br>
+		<ul>
+		<li>add external data or content (e.g. your CMS, static files, a REST API) to your Gatsby GraphQL data</li>
+		<li>transform data from other formats (e.g. Markdown, YAML, CSV) to JSON objects</li>
+		<li>add third-party services (e.g. Google Analytics, Instagram) to your site</li>
+		<li>add bundled, pre-configured functionality with themes
+		do anything you can dream up!</li>
+		</ul></p>
+		<h3>Using Gatsby Plugins through GatsbyHub in your new Gatsby site</h3>
+		<p>
+		Gatsby plugins are Node.js packages, so you can install them like other packages in node, and with GatsbyHub, you can now use the install button in the plugins menu sidebar.
+		<br>
+		Then update your gatsby-config.js file to include the new plugin in your plugins array. 
+		<br>
+		<br>
+		<a href="https://www.gatsbyjs.com/docs/using-a-plugin-in-your-site/">Read more about gatsby pluggins</a>
+		</p>
+		`;
 
 		// close the webview when not looking at it
 		panel.onDidChangeViewState((e) => {
